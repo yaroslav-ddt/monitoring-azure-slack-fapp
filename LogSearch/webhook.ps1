c082181427a18f7ef1d62a6e5be6e5313bef39e7
Yaroslav Yakushev <yaroslav.yakushev@sbb.ch>
2023-07-28 13:50:16 +0200

    ps function

--- LogSearch/webhook.ps1
+++ LogSearch/webhook.ps1
@@ -51,12 +51,9 @@ $SLACK_MSG_TEMPLATE = @"
 $alert = $request.body
 
 $SLACK_MSG_TEMPLATE.blocks[0].text.text = "The pod $($alert.data.alertContext.condition.allOf[0].dimensions[0].value) has been killed"
-$SLACK_MSG_TEMPLATE.attachments[0].blocks[0].text.text = $alert.data.alertContext.condition.allOf[0] | convertto-json  -Depth 10
+$SLACK_MSG_TEMPLATE.attachments[0].blocks[0].text.text = "linkToFilteredSearchResultsUI:"+$alert.data.alertContext.condition.allOf[0].linkToFilteredSearchResultsUI
 
-write-host $SLACK_MSG_TEMPLATE.attachments[0].blocks[0].text.text
-write-host $SLACK_MSG_TEMPLATE.blocks[0].text.text
-
-Invoke-WebRequest -Method POST -Headers @{"Content-Type" = "application/json"} -Body ($SLACK_MSG_TEMPLATE|convertto-json -Depth 10) -Uri $env:SLACK_HOOK_URL
+Invoke-WebRequest -Method POST -ContentType "application/json" -Body ($SLACK_MSG_TEMPLATE|convertto-json -Depth 10) -Uri $env:SLACK_HOOK_URL
 #Extract projected fields from Log Search Alert
 # $computer = $alert.body.data.alertContext.SearchResults.tables.rows[0]
 # $svcname = $alert.body.data.alertContext.SearchResults.tables.rows[1]
