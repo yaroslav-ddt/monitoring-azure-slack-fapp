7866b9ba36de822bf9186b32dd3634309627db77
Yaroslav Yakushev <yaroslav.yakushev@sbb.ch>
2023-07-28 08:53:34 +0200

    ps function

--- LogSearch/webhook.ps1
+++ LogSearch/webhook.ps1
@@ -10,16 +10,15 @@ Write-Host "PowerShell HTTP trigger function processed a request."
 $alert = $request.body
 
 #extract Affected cI
-$affectedCI = $alert.body.data.alertContext.AffectedConfigurationItems
-write-host "Affected CI" $affectedCI
+write-host "Alert Body" $alert
 
 
 #Extract projected fields from Log Search Alert
-$computer = $alert.body.data.alertContext.SearchResults.tables.rows[0]
-$svcname = $alert.body.data.alertContext.SearchResults.tables.rows[1]
-$svcstate = $alert.body.data.alertContext.SearchResults.tables.rows[2]
-$svcdisplayname = $alert.body.data.alertContext.SearchResults.tables.rows[3]
-$TimeGenerated = $alert.body.data.alertContext.SearchResults.tables.rows[4]
+# $computer = $alert.body.data.alertContext.SearchResults.tables.rows[0]
+# $svcname = $alert.body.data.alertContext.SearchResults.tables.rows[1]
+# $svcstate = $alert.body.data.alertContext.SearchResults.tables.rows[2]
+# $svcdisplayname = $alert.body.data.alertContext.SearchResults.tables.rows[3]
+# $TimeGenerated = $alert.body.data.alertContext.SearchResults.tables.rows[4]
 
-write-host "Computer" $computer "svc name" $svcname "svcstate" $svcstate "svc displayname" $svcdisplayname "TimeGenerated" $timegenerated
+# write-host "Computer" $computer "svc name" $svcname "svcstate" $svcstate "svc displayname" $svcdisplayname "TimeGenerated" $timegenerated
         
