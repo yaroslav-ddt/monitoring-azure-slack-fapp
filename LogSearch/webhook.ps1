ec8157a3f442425f838476edaf99334dcb641b88
Yaroslav Yakushev <yaroslav.yakushev@sbb.ch>
2023-07-28 14:01:22 +0200

    ps function

--- LogSearch/webhook.ps1
+++ LogSearch/webhook.ps1
@@ -50,8 +50,8 @@ $SLACK_MSG_TEMPLATE = @"
 
 $alert = $request.body
 
-$SLACK_MSG_TEMPLATE.blocks[0].text.text = $alert.data.essentials.alertRule+": The pod $($alert.data.alertContext.condition.allOf[0].dimensions[0].value) has been killed\n"
-$SLACK_MSG_TEMPLATE.blocks[2].text.text = "Please find log query result by the link:"
+$SLACK_MSG_TEMPLATE.blocks[0].text.text = $alert.data.essentials.alertRule+": "+$alert.data.alertContext.condition.allOf[0].dimensions[0].value
+$SLACK_MSG_TEMPLATE.blocks[2].text.text = "Please find more details by the link:"
 $SLACK_MSG_TEMPLATE.attachments[0].blocks[0].text.text = $alert.data.alertContext.condition.allOf[0].linkToFilteredSearchResultsUI
 
 Invoke-WebRequest -Method POST -ContentType "application/json" -Body ($SLACK_MSG_TEMPLATE|convertto-json -Depth 10) -Uri $env:SLACK_HOOK_URL
