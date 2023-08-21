ac2dbed92fb07795440216bea4c6a03c2a25441f
Yaroslav Yakushev <yaroslav.yakushev@sbb.ch>
2023-08-21 13:47:17 +0200

    added logs for alert body and slack template

--- LogSearch/webhook.ps1
+++ LogSearch/webhook.ps1
@@ -49,11 +49,13 @@ $SLACK_MSG_TEMPLATE = @"
 "@ | convertfrom-json
 
 $alert = $request.body
-
+write-host "==============Alert Body======================"
+write-host ($alert | convertto-json -Depth 10)
 $SLACK_MSG_TEMPLATE.blocks[0].text.text = $alert.data.essentials.alertRule+": "+$alert.data.alertContext.condition.allOf[0].dimensions[0].value
 $SLACK_MSG_TEMPLATE.blocks[2].text.text = "Please find more details by the link:"
 $SLACK_MSG_TEMPLATE.attachments[0].blocks[0].text.text = $alert.data.alertContext.condition.allOf[0].linkToFilteredSearchResultsUI
 
-Invoke-WebRequest -Method POST -ContentType "application/json" -Body ($SLACK_MSG_TEMPLATE|convertto-json -Depth 10) -Uri $env:SLACK_HOOK_URL
-
+#Invoke-WebRequest -Method POST -ContentType "application/json" -Body ($SLACK_MSG_TEMPLATE|convertto-json -Depth 10) -Uri $env:SLACK_HOOK_URL
+write-host "===============SLACK========================="
+write-host ($SLACK_MSG_TEMPLATE | convertto-json -Depth 10)
         
